9939281ae9698b415863704ed04dbda9e4cda7ee
Gary Burton <garypburton@gmail.com>
2018-11-04 19:28:37 +0000

    added some action types and constants for home scene

---
+++ src/scenes/home/constants.js
@@ -0,0 +1,4 @@
+export const HOTEL_DATA_IDLE = 'hoteldata:status:idle';
+export const HOTEL_DATA_PENDING = 'hoteldata:status:pending';
+export const HOTEL_DATA_COMPLETE = 'hoteldata:status:complete';
+export const HOTEL_DATA_FAILED = 'hoteldata:status:failed';
---
+++ src/scenes/home/duck/types.js
@@ -0,0 +1,2 @@
+export const HOTEL_DATA_STATUS = 'hoteldata:status';
+export const HOTEL_DATA_RESULT = 'hoteldata:result';
