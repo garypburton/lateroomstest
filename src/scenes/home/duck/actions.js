6c894e40bdbc893df0271944cd2c29a927c35232
Gary Burton <garypburton@gmail.com>
2018-11-04 19:41:55 +0000

    made actions pass tests

--- src/scenes/home/duck/actions.js
+++ src/scenes/home/duck/actions.js
@@ -1,11 +1,26 @@
 import * as types from './types';
 import * as constants from '../constants';
 
-const willFetchHotelData = () => ({});
+const willFetchHotelData = () => ({
+  type: types.HOTEL_DATA_STATUS,
+  payload: {
+    status: constants.HOTEL_DATA_PENDING,
+  },
+});
 
-const didFetchHotelData = () => ({});
+const didFetchHotelData = () => ({
+  type: types.HOTEL_DATA_STATUS,
+  payload: {
+    status: constants.HOTEL_DATA_COMPLETE,
+  },
+});
 
-const failedToFetchHotelData = () => ({});
+const failedToFetchHotelData = () => ({
+  type: types.HOTEL_DATA_STATUS,
+  payload: {
+    status: constants.HOTEL_DATA_FAILED,
+  },
+});
 
 export default {
   willFetchHotelData,
